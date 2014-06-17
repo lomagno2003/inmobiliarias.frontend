6e3148962163ef9510b9196b53c1b3fd32736714
lomagno2003 <lomagno2003@gmail.com>
2014-06-17 14:51:43 -0300

    CLOSED - task 16: Refactor RepositoryServices making a Hierarchy and Creating a factory for the different subclasses https://github.com/lomagno2003/inmobiliarias.frontend/issues/issue/16

--- js/services/PropietariosService.js
+++ js/services/PropietariosService.js
@@ -2,10 +2,13 @@ var myApp = angular.module('myApp');
 
 myApp.service('PropietariosService', function($resource) {
 	this.viewListStructure = {
-			title : "Detalles de la propiedad",
+			title : "Detalles del propietario",
 			columns : [ {
 				'columnId' : 'nombre',
 				'columnName' : 'Nombre'
+			}, {
+				'columnId' : 'dni',
+				'columnName' : 'DNI'
 			}]	
 	};
 	
@@ -14,6 +17,26 @@ myApp.service('PropietariosService', function($resource) {
 		fields : [ {
 			'fieldId' : 'nombre',
 			'fieldName' : 'Nombre',
+			'fieldType' : 'text'
+		}, {
+			'fieldId' : 'dni',
+			'fieldName' : 'DNI',
+			'fieldType' : 'text'
+		}, {
+			'fieldId' : 'direccion',
+			'fieldName' : 'Direccion',
+			'fieldType' : 'text'
+		}, {
+			'fieldId' : 'telefonoFijo',
+			'fieldName' : 'Telefono',
+			'fieldType' : 'text'
+		}, {
+			'fieldId' : 'telefonoMovil',
+			'fieldName' : 'Movil',
+			'fieldType' : 'text'
+		}, {
+			'fieldId' : 'email',
+			'fieldName' : 'Correo Electronico',
 			'fieldType' : 'text'
 		}, {
 			'fieldId' : 'propiedad',
