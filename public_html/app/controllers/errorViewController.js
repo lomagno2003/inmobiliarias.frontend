1c4f68435d62797999ce7e37af0f2d5617adb9cd
lomagno2003 <lomagno2003@gmail.com>
2014-06-26 16:38:30 -0300

    CLOSED - task 34: Make error view looks better https://github.com/lomagno2003/inmobiliarias.frontend/issues/issue/34

--- public_html/app/controllers/errorViewController.js
+++ public_html/app/controllers/errorViewController.js
@@ -2,5 +2,24 @@ var myApp = angular.module('myApp');
 
 myApp.controller('errorViewController', function($scope,$routeParams) {	
 		$scope.errorId = $routeParams.errorId;
+		
+		switch($scope.errorId){
+		case 'rest_server_offline':
+			$scope.errorTitle = "El servidor REST se encuentra caido";
+			$scope.errorMessage = "No se pudo establecer la comunicaciñón con el servidor, intentelo mas tarde";
+			break;
+		case 'jasper_server_offline':
+			$scope.errorTitle = "El servidor Jasper se encuentra caido";
+			$scope.errorMessage = "No se pudo establecer la comunicaciñón con el servidor, intentelo mas tarde";
+			break;
+		case 'not_found':
+			$scope.errorTitle = "Pagina no encontrada";
+			$scope.errorMessage = "No se pudo acceder a la pagina solicitada";
+			break;
+		default:
+			$scope.errorTitle = "Error desconocido";
+			$scope.errorMessage = "Se ah producido un error desconocido, contactese con el administrador";
+			break;
+		};
 	}
 );
